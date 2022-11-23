b0e11f9932a86da35a80ddbf81a9d88f30bd70a9
Eminence <eminence@rawbank.cd>
2022-11-23 08:21:42 +0100

    initialisation

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "",
+    database: "",
+    host: "",
+    dialect: "mysql",
+  },
+  production: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    dialect: "mysql",
+  },
+};
