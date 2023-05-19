3fbbfddb315f4f73465e6dd8719207f80c8fa269
Eminence <eminence@rawbank.cd>
2023-05-19 15:34:20 +0100

    added dependencies

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+// app.use("/api/v1/user", cors(options), require("./routes/userRoute"));
+// app.use("/api/v1/customer", cors(options), require("./routes/customerRoute"));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
