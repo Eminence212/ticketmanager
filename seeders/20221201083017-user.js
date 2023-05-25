b09011a18817796b1738282e17ab98272138e46d
Eminence <eminence@rawbank.cd>
2023-05-25 09:04:41 +0100

    fit

--- seeders/20221201083017-user.js
+++ seeders/20221201083017-user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 /** @type {import('sequelize-cli').Migration} */
-const { USER, PASSWORD, ROLE } = process.env;
+const { USER, PASSWORD, ROLE, USER1, PASSWORD1, ROLE1 } = process.env;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
@@ -10,6 +10,13 @@ module.exports = {
         name: USER.toUpperCase(),
         password: await bcrypt.hash(PASSWORD, 12),
         role: parseInt(ROLE),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: USER1.toUpperCase(),
+        password: await bcrypt.hash(PASSWORD1, 12),
+        role: parseInt(ROLE1),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
